86857d7b9a5751e2b8bc7372d80a285b1378eeaf
Brian Petkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2023-04-20 16:54:29 -0400

    fix example

--- ExampleRequiresGen.py
+++ ExampleRequiresGen.py
@@ -3,13 +3,13 @@ from PandasGenerativeDataFrame.Generators import SMAGenerator
 from PandasGenerativeDataFrame import ColumnGenerator
 from typing import Dict
 
-class ExampleRequiresGen(ColumnGenerator)
-    def __init__(self) -> None
+class ExampleRequiresGen(ColumnGenerator):
+    def __init__(self) -> None:
         pass
 
-    def requires(self) -> Dict[str, ColumnGenerator]
-        return {SMA_10: SMAGenerator(10)}
+    def requires(self) -> Dict[str, ColumnGenerator]:
+        return {"SMA_10": SMAGenerator(10)}
     
-    def gen(self, index, row pd.Series)
+    def gen(self, index, row pd.Series):
         return row.SMA_10+1
 
