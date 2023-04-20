882d749e9bca59ee9b4af43528cdd3857ec4b958
Brian Petkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2023-04-20 16:23:44 -0400

    add ExampleRequiresGen

---
+++ ExampleRequiresGen.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from PandasGenerativeDataFrame.Generators import SMAGenerator
+from PandasGenerativeDataFrame import ColumnGenerator
+from typing import Dict
+
+class ExampleRequiresGen(ColumnGenerator)
+    def __init__(self) -> None
+        pass
+
+    def requires(self) -> Dict[str, ColumnGenerator]
+        return {SMA_10: SMAGenerator(10)}
+    
+    def gen(self, index, row pd.Series)
+        return row.SMA_10+1
+
